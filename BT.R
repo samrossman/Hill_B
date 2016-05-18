39b9a183f879e7fb5ebb0f66e842ac6142c93e95
samrossman <rossman.sam@gmail.com>
2016-05-17 20:54:27 -0400

    modified wd for use on desktop

--- BT.R
+++ BT.R
@@ -1,11 +1,9 @@
-setwd("C:\\Users\\samro\\OneDrive\\Hill_Betaines")
-table1<-read.csv("file:///C:/Users/samro/OneDrive/Hill_Betaines/Nonresident Analysis Table A.csv",header=TRUE)
-table2<-read.csv("file:///C:/Users/samro/OneDrive/Hill_Betaines/Nonresident Analysis Table B.csv",header=TRUE)
+setwd("C:\\Users\\samro\\OneDrive\\Hill_B")
+setwd("C:\\Users\\rossm_000\\SkyDrive\\Hill_B")
+table1<-read.csv("Nonresident Analysis Table A.csv",header=TRUE)
+table2<-read.csv("Nonresident Analysis Table B.csv",header=TRUE)
 data<-cbind(table1,table2[,10:183])
-
-c(data)[1],c(data)[2])
 sp<-colnames(data)
-c(unlist(data[,10:363]))
 
 X<-NA
 for (i in 10:363){
@@ -14,7 +12,7 @@ for (i in 10:363){
   temp<-cbind(data[,1:9],species,time,data[,i])
   X<-rbind(X,temp)
 }
-X<-X[-1,]
+X<-X[complete.cases(X),]
 
 species.code<-as.data.frame(cbind(levels(as.factor(X$species)),levels(as.factor(as.numeric(X$species)))))
 control.code<-as.data.frame(cbind(levels(as.factor(X$Treatment)),levels(as.factor(as.numeric(X$Treatment)))))
